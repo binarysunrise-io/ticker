427119fb8e7833d2afd2ad2db3cc37d42ea87335
Jesse Kempf <jessekempf@gmail.com>
2018-01-31 20:12:21 -0800

    Add logging support to CLI skeleton

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import           Data.Semigroup      ((<>))
-import           Lib.Types           (Amplitude (..), StreamName (..))
+import           Control.Monad.Logger (LogLevel (..), LogSource, filterLogger,
+                                       runStderrLoggingT)
+import           Data.Semigroup       ((<>))
+import           Lib.Types            (Amplitude (..), StreamName (..))
 import           Options.Applicative
 
 reportArgsParser :: Parser (StreamName, Amplitude)
@@ -13,11 +17,41 @@ reportArgsParser =
     streamNameParser = StreamName <$> strArgument   (metavar "STREAMNAME" <> help "Stream to report for")
     amplitudeParser  = Amplitude  <$> argument auto (metavar "AMPLITUDE"  <> help "Amplitude to use")
 
+loggingFlag :: Parser (LogSource -> LogLevel -> Bool)
+loggingFlag = option
+                (maybeReader namedFilter)
+                (short 'l'
+                  <> long "logging"
+                  <> help "specify logging level (default | debug | trace)"
+                  <> metavar "LEVEL"
+                  <> value defaultLogFilter
+                  <> showDefaultWith (const "default")
+                )
+    where
+      namedFilter "default" = Just defaultLogFilter
+      namedFilter "debug"   = Just debugLogFilter
+      namedFilter "trace"   = Just traceLogFilter
+      namedFilter _         = Nothing
+
+      defaultLogFilter _ (LevelOther "Trace") = False
+      defaultLogFilter _ level                = level >= LevelInfo
+
+      debugLogFilter "AWS Client" (LevelOther "Trace") = False
+      debugLogFilter "AWS Client" level                = level >= LevelInfo
+      debugLogFilter _            _                    = True
+
+      traceLogFilter _ _                               = True
+
 main :: IO ()
-main = customExecParser pref opts >>= print
+main = do
+  (loggingFilter, reportArgs) <- customExecParser pref opts
+
+  runStderrLoggingT . filterLogger loggingFilter $
+    undefined reportArgs
+
   where
     pref = prefs showHelpOnEmpty
-    opts = info (reportArgsParser <**> helper)
+    opts = info (((,) <$> loggingFlag <*> reportArgsParser) <**> helper)
             (
               fullDesc
               <> progDesc "Report a measurement to STREAMNAME of AMPLITUDE"
